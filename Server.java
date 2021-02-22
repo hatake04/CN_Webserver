642cc23a93eb423ba7b7f3605d8fa89a5ae5afb8
hatake04 <michaelangelo002@hotmail.com>
2021-02-21 20:45:16 -0500

    Update Server.java

--- Server.java
+++ Server.java
@@ -5,32 +5,42 @@ public class WebServer
 {
     public static void main(String[] args)
     {
-        int portNumber = 0;
-
+        int portNumber;
+        String inputString;
         if(args.length != 1)
             portNumber = 80;    //default socket to listen
         else
             portNumber = Integer.parseInt(args[0]); //socket number to listen provided by terminal/cmd
-        
-        try{
 
-            ServerSocket serverSocket = new ServerSocket(portNumber); //creates a socket to listen at portnumber
-            Socket clientSocket = serverSocket.accept();     //accepts connection from client          
+        try(ServerSocket serverSocket = new ServerSocket(portNumber); //creates a socket to listen at portnumber
+            Socket clientSocket = serverSocket.accept();     //accepts connection from client   
+              
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  // reads input from client
-            String inputString = in.readLine();
-            String[] str = inputString.split(" "); //splits string line based on white space
-
-            if(str[0].equals("GET"))
-            {}
-            else
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);)
             {
-                System.err.println("HTTP's method GET not found\nTerminating connection...");
-                System.exit(1);
+            System.out.println("Connected");     
+            String line;
+            
+            while((line = in.readLine()) != null);
+            {
+                inputString = line;
+                String[] str = inputString.split(" "); //splits string line based on white space
+                File file = new File(str[1]); // str1 element has the document's path/name to be opened
+            
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String docLine;
+                while((docLine = br.readLine()) != null)
+                    out.println(docLine);
+                br.close();
             }
-
+                
+            } 
+        catch(IOException e)
+        {
+            System.out.println("Exception caught when trying to listen on port " + portNumber
+            + " or listening for a connection");
+            System.out.println(e.getMessage());
         }
 
-        catch(){
-        }
     }
 }
