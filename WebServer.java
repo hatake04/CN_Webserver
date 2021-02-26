b48e5bf9491b916250c217bf9e38b6b45e767889
hatake04 <michaelangelo002@hotmail.com>
2021-02-26 08:23:03 -0500

    Update WebServer.java

--- WebServer.java
+++ WebServer.java
@@ -5,35 +5,26 @@ public class WebServer
 {
     public static void main(String[] args)
     {
-        int portNumber;
+        int portNumber; //port number where server will listen for client requests
+        
+        //System.out.println("Working Directory = " + System.getProperty("user.dir")); prints your current path directory (put document here)
+
         if(args.length != 1)
-            portNumber = 80;    //default socket to listen
+            portNumber = 80;    //default port number to listen if client doesn't give one
         else
-            portNumber = Integer.parseInt(args[0]); //socket number to listen provided by terminal/cmd
+            portNumber = Integer.parseInt(args[0]); //port number to listen provided int the terminal/cmd 
 
-        try(ServerSocket serverSocket = new ServerSocket(portNumber); //creates a socket to listen at portnumber
-            Socket clientSocket = serverSocket.accept();     //accepts connection from client   
-              
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  // reads input from client
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);)
+            //creates a socket to listen at port number
+        try(ServerSocket serverSocket = new ServerSocket(portNumber);) 
         {
-                out.println("Connection established..."); 
-                String line;
-
-                while((line = in.readLine()) != null); //reads from user's keyboard until end of input character (CTRL-C)
-                {
-                    String[] str = line.split(" "); //splits string line based on white space --bug in this line--
-                    BufferedReader br = new BufferedReader(new FileReader(str[1]));  //element 1 holds the name of the file
-                    String docLine;
-                    while((docLine = br.readLine()) != null) //while loop reads the content of the document
-                        out.println(docLine); //sends the contents line by line to client
-                }
-        } 
+            //infinite loop to listen for incoming client requests. it accepts multiples connections by creating the necessary threads
+            while(true)
+                new WebServerThread(serverSocket.accept()).start(); //the statement is not executed until a connection is made with a client
+        }
         catch(IOException e)
         {
             System.out.println("Exception caught when trying to listen on port " + portNumber
             + " or listening for a connection");
-            System.out.println(e.getMessage());
         }
     }
 }
