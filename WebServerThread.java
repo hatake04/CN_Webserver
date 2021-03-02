8cfbc9a1aa3cf55817789b2a4be2fcfa33373b4e
hatake04 <michaelangelo002@hotmail.com>
2021-03-01 22:09:32 -0500

    Update WebServerThread.java

--- WebServerThread.java
+++ WebServerThread.java
@@ -42,8 +42,7 @@ public class WebServerThread extends Thread {
                 System.out.println("Preparing to send document...");
                 try
                 {
-                    String subStr = str[1].subString(1);
-                    BufferedReader br = new BufferedReader(new FileReader(subStr);  //element 1 holds the name of the file
+                    BufferedReader br = new BufferedReader(new FileReader(str[1]));;  //element 1 holds the name of the file
                     while((docLine = br.readLine()) != null) //while loop reads the content of the document
                         doc += docLine; //Stores the whole document
                     out.println((doc += "\r\n")); //sends the contents line by line to client
